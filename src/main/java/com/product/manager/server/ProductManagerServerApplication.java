16817eb93c5b7460914e6274d1cdd9163db3b153
RBarnabass <rastafarimood@gmail.com>
2020-08-14 13:04:22 +0300

    Create product manager application skeleton.

---
+++ src/main/java/com/product/manager/server/ProductManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.product.manager.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/manager/server/ProductManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.manager.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
