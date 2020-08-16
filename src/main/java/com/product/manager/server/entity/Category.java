6c40846fc8da13a007b4bef56e56ea944c819c4d
RBarnabass <rastafarimood@gmail.com>
2020-08-16 10:50:29 +0300

    Models improvements.

--- src/main/java/com/product/manager/server/entity/Category.java
+++ src/main/java/com/product/manager/server/entity/Category.java
@@ -1,12 +1,27 @@
 package com.product.manager.server.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.LazyCollection;
 
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import java.util.List;
+
+import static com.product.manager.server.constant.DigitConstant.MAX_CATEGORY_NAME_LENGTH;
+import static com.product.manager.server.constant.Table.CATEGORIES;
+import static org.hibernate.annotations.LazyCollectionOption.EXTRA;
 
 @Data
-public class Category {
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = CATEGORIES)
+public class Category extends BaseEntity {
 
-    @Id
-    private Long id;
+    @Column(unique = true, nullable = false, length = MAX_CATEGORY_NAME_LENGTH)
+    private String name;
+
+    @LazyCollection(EXTRA)
+    @ManyToMany(mappedBy = CATEGORIES)
+    private List<Product> products;
 }
--- src/main/java/com/product/manager/server/entity/Product.java
+++ src/main/java/com/product/manager/server/entity/Product.java
@@ -1,12 +1,37 @@
 package com.product.manager.server.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static com.product.manager.server.constant.DigitConstant.MAX_PRODUCT_MODEL_LENGTH;
+import static com.product.manager.server.constant.DigitConstant.MAX_PRODUCT_NAME_LENGTH;
+import static com.product.manager.server.constant.Table.PRODUCTS;
 
 @Data
-public class Product {
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = PRODUCTS)
+public class Product extends BaseEntity {
 
-    @Id
-    private Long id;
+    @Column(nullable = false, length = MAX_PRODUCT_NAME_LENGTH)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = MAX_PRODUCT_MODEL_LENGTH)
+    private String model;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne
+    private Currency currency;
+
+    @ManyToMany
+    private List<Category> categories;
 }
